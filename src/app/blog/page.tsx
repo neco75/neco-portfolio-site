f3d30c5dc9353f4904fc650ed1b4cf1b3d12d374
sagara_rikuto <sagara_rikuto@plise.co.jp>
2023-10-17 12:17:28 +0900

    ブログページレスポンシブ

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,108 +3,219 @@ import NextLink from "next/link";
 
 const BlogPage = () => {
     return (
-        <Box mx="auto" p={4}>
-            <Heading as="h1" size="xl" mb={4} textAlign="center">
-                Blog
-            </Heading>
-            <Grid templateColumns="repeat(4, 1fr)" gap={4}>
-                <Flex direction="column">
-                    <Link as={NextLink} href="/blog/1">
-                        <Flex direction="column">
-                            <Image
-                                src="https://via.placeholder.com/150"
-                                alt="blog thumbnail"
-                                mb={4}
-                            />
-                            <Box>
-                                <Heading as="h2" size="lg">
-                                    Blog Post Title
-                                </Heading>
-                                <Text fontSize="md" color="gray.500">
-                                    Created on: 2022-01-01
-                                </Text>
-                            </Box>
-                        </Flex>
-                    </Link>
-                </Flex>
-                <Flex direction="column">
-                    <Link as={NextLink} href="/blog/2">
-                        <Flex direction="column">
-                            <Image
-                                src="https://via.placeholder.com/150"
-                                alt="blog thumbnail"
-                                mb={4}
-                            />
-                            <Box>
-                                <Heading as="h2" size="lg">
-                                    Blog Post Title
-                                </Heading>
-                                <Text fontSize="md" color="gray.500">
-                                    Created on: 2022-01-02
-                                </Text>
-                            </Box>
-                        </Flex>
-                    </Link>
-                </Flex>
-                <Flex direction="column">
-                    <Link as={NextLink} href="/blog/3">
-                        <Flex direction="column">
-                            <Image
-                                src="https://via.placeholder.com/150"
-                                alt="blog thumbnail"
-                                mb={4}
-                            />
-                            <Box>
-                                <Heading as="h2" size="lg">
-                                    Blog Post Title
-                                </Heading>
-                                <Text fontSize="md" color="gray.500">
-                                    Created on: 2022-01-03
-                                </Text>
-                            </Box>
-                        </Flex>
-                    </Link>
-                </Flex>
-                <Flex direction="column">
-                    <Link as={NextLink} href="/blog/4">
-                        <Flex direction="column">
-                            <Image
-                                src="https://via.placeholder.com/150"
-                                alt="blog thumbnail"
-                                mb={4}
-                            />
-                            <Box>
-                                <Heading as="h2" size="lg">
-                                    Blog Post Title
-                                </Heading>
-                                <Text fontSize="md" color="gray.500">
-                                    Created on: 2022-01-04
-                                </Text>
-                            </Box>
-                        </Flex>
-                    </Link>
-                </Flex>
-                <Flex direction="column">
-                    <Link as={NextLink} href="/blog/5">
-                        <Flex direction="column">
-                            <Image
-                                src="https://via.placeholder.com/150"
-                                alt="blog thumbnail"
-                                mb={4}
-                            />
-                            <Box>
-                                <Heading as="h2" size="lg">
-                                    Blog Post Title
-                                </Heading>
-                                <Text fontSize="md" color="gray.500">
-                                    Created on: 2022-01-05
-                                </Text>
-                            </Box>
-                        </Flex>
-                    </Link>
-                </Flex>
-            </Grid>
+        <Box>
+            <Flex>
+                <Box mx="auto" p={4} display={{ base: "none", md: "flex" }}>
+                    <Box>
+                        <Heading as="h1" size="xl" mb={4} textAlign="center">
+                            Blog
+                        </Heading>
+                        <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/1">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-01
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/2">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-02
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/3">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-03
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/4">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-04
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/5">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-05
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Box mx="auto" p={4} display={{ base: "block", md: "none" }}>
+                    <Box>
+                        <Heading as="h1" size="xl" mb={4} textAlign="center">
+                            Blog
+                        </Heading>
+                        <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/1">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-01
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/2">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-02
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/3">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-03
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/4">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-04
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                            <Flex direction="column">
+                                <Link as={NextLink} href="/blog/5">
+                                    <Flex direction="column">
+                                        <Image
+                                            src="https://via.placeholder.com/150"
+                                            alt="blog thumbnail"
+                                            mb={4}
+                                        />
+                                        <Box>
+                                            <Heading as="h2" size="lg">
+                                                Blog Post Title
+                                            </Heading>
+                                            <Text fontSize="md" color="gray.500">
+                                                Created on: 2022-01-05
+                                            </Text>
+                                        </Box>
+                                    </Flex>
+                                </Link>
+                            </Flex>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Flex>
         </Box>
+        
     );
 };
 
