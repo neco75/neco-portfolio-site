189d6a0cebee34d932a59e5b0d686bb78b1be5a5
sagara_rikuto <sagara_rikuto@plise.co.jp>
2023-10-17 13:19:41 +0900

    GALLERY　レスポンシブ化

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,75 +1,72 @@
-import { Box, Grid, GridItem, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Heading, Image, Link, Text } from "@chakra-ui/react";
 import { NextPage } from "next";
-
-interface GalleryItem {
-    id: string;
-    imageUrl: string;
-    title: string;
-    description: string;
-}
-
-const galleryItems: GalleryItem[] = [
-    {
-        id: "1",
-        imageUrl: "https://via.placeholder.com/300x300",
-        title: "Item 1",
-        description: "This is the first item in the gallery.",
-    },
-    {
-        id: "2",
-        imageUrl: "https://via.placeholder.com/300x300",
-        title: "Item 2",
-        description: "This is the second item in the gallery.",
-    },
-    {
-        id: "3",
-        imageUrl: "https://via.placeholder.com/300x300",
-        title: "Item 3",
-        description: "This is the third item in the gallery.",
-    },
-    {
-        id: "4",
-        imageUrl: "https://via.placeholder.com/300x300",
-        title: "Item 4",
-        description: "This is the fourth item in the gallery.",
-    },
-    {
-        id: "5",
-        imageUrl: "https://via.placeholder.com/300x300",
-        title: "Item 5",
-        description: "This is the fifth item in the gallery.",
-    },
-    {
-        id: "6",
-        imageUrl: "https://via.placeholder.com/300x300",
-        title: "Item 6",
-        description: "This is the sixth item in the gallery.",
-    },
-    {
-        id: "7",
-        imageUrl: "https://via.placeholder.com/300x300",
-        title: "Item 6",
-        description: "This is the sixth item in the gallery.",
-    },
-];
+import NextLink from "next/link";
 
 const GalleryPage: NextPage = () => {
     return (
-        <Box p={4}>
-            <Heading as="h1" size="xl" mb={4} textAlign="center">
-                Gallery
-            </Heading>
-            <Grid templateColumns="repeat(5, 1fr)" gap={6} justifyItems="center" alignItems="center">
-                {galleryItems.map((item) => (
-                    <GridItem key={item.id}>
-                        <Image src={item.imageUrl} alt={item.title} />
-                        <Text mt={2} fontWeight="bold">
-                            {item.title}
-                        </Text>
-                        <Text>{item.description}</Text>
-                    </GridItem>
-                ))}
-            </Grid>
+        <Box>
+            <Flex>
+                <Box mx="auto" p={4} display={{ base: "none", md: "flex" }}>
+                    <Box>
+                        <Heading as="h1" size="xl" mb={4} textAlign="center">
+                            Gallery
+                        </Heading>
+                        <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+                            {[1, 2, 3, 4, 5].map((postId) => (
+                                <Flex key={postId} direction="column">
+                                    <Link as={NextLink} href={`/blog/${postId}`}>
+                                        <Flex direction="column">
+                                            <Image
+                                                src="https://via.placeholder.com/350"
+                                                alt="blog thumbnail"
+                                                mb={4}
+                                            />
+                                            <Box>
+                                                <Heading as="h2" size="lg">
+                                                    Gallery Item Title
+                                                </Heading>
+                                                <Text fontSize="md" color="gray.500">
+                                                    Created on: 2022-01-0{postId}
+                                                </Text>
+                                            </Box>
+                                        </Flex>
+                                    </Link>
+                                </Flex>
+                            ))}
+                        </Grid>
+                    </Box>
+                </Box>
+                <Box mx="auto" p={4} display={{ base: "block", md: "none" }}>
+                    <Box>
+                        <Heading as="h1" size="xl" mb={4} textAlign="center">
+                            Gallery
+                        </Heading>
+                        <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+                            {[1, 2, 3, 4, 5].map((postId) => (
+                                <Flex key={postId} direction="column">
+                                    <Link as={NextLink} href={`/blog/${postId}`}>
+                                        <Flex direction="column">
+                                            <Image
+                                                src="https://via.placeholder.com/350"
+                                                alt="blog thumbnail"
+                                                mb={4}
+                                            />
+                                            <Box>
+                                                <Heading as="h2" size="lg">
+                                                    Gallery Item Title
+                                                </Heading>
+                                                <Text fontSize="md" color="gray.500">
+                                                    Created on: 2022-01-0{postId}
+                                                </Text>
+                                            </Box>
+                                        </Flex>
+                                    </Link>
+                                </Flex>
+                            ))}
+                        </Grid>
+                    </Box>
+                </Box>
+            </Flex>
         </Box>
     );
 };
