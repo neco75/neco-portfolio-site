31f61d11145f2212b4eb62e9c3a1e7160f68b408
sagara_rikuto <sagara_rikuto@plise.co.jp>
2023-10-21 17:21:52 +0900

    blog fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,47 +1,79 @@
-import { Box, Heading, Text, Flex, Image, Grid, List, Link } from "@chakra-ui/react";
+import { Box, Heading, Text, Flex, Image, Grid, Link } from "@chakra-ui/react";
 import { getList } from "../../libs/client";
+import "./style.scss";
 
 export default async function StaticPage() {
- const { contents } = await getList();
+  const { contents } = await getList();
 
- if (!contents || contents.length === 0) {
+  if (!contents || contents.length === 0) {
     return <h1>No contents</h1>;
- }
+  }
 
- return (
+  return (
     <Box p={4}>
-        <Heading as="h1" size="xl" mb={4} textAlign="center">
-                Blog
-        </Heading>
-     <Grid templateColumns={{ base: "repeat(auto-fit, minmax(300px, 1fr))" , md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)", xl: "repeat(4, 1fr)" }} gap={4} p={4}>
+      <Heading as="h1" size="xl" mb={4} textAlign="center">
+        Blog
+      </Heading>
+      <Grid
+        templateColumns={{
+          base: "repeat(auto-fit, minmax(300px, 1fr))",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+          xl: "repeat(4, 1fr)",
+        }}
+        gap={4}
+        p={4}
+      >
         {contents.map((post) => {
-            return (
-                <Box key={post.id} bg="white" borderRadius="md" overflow="hidden" boxShadow="md" position="relative" border={"1px"} borderColor={"gray.200"}>
-                    <Link href={`/blog/${post.id}`} textDecoration="none">
-                        <Flex direction="column">
-                            <Image
-                                src={post.eyecatch?.url}
-                                alt="blog thumbnail"
-                                mb={4}
-                                width="100%"
-                                objectFit="cover"
-                                height="200"
-                                backgroundColor={"gray.100"}
-                            />
-                            <Box height="200">
-                                <Heading as="h2" size="md" p={4}>
-                                    {post.title.slice(0, 50)}{post.title.length > 50 ? "..." : ""}
-                                </Heading>
-                            </Box>
-                        </Flex>
-                        <Text fontSize="md" color="gray.500" position="absolute" bottom={0} right={0} p={4}>
-                            {new Date(post.createdAt).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })}
-                        </Text>
-                    </Link>
-                </Box>
-            );
+          return (
+            <Box
+              className="blog-card"
+              key={post.id}
+              bg="white"
+              borderRadius="md"
+              overflow="hidden"
+              boxShadow="md"
+              position="relative"
+              border={"1px"}
+              borderColor={"gray.200"}
+            >
+              <Link href={`/blog/${post.id}`} textDecoration="none">
+                <Flex direction="column">
+                  <Image
+                    className="blog-card-thumbnail"
+                    src={post.eyecatch?.url}
+                    alt="blog thumbnail"
+                    mb={4}
+                    width="100%"
+                    objectFit="cover"
+                    height="200"
+                    backgroundColor={"gray.100"}
+                  />
+                  <Box height="200" className="blog-card-title">
+                    <Heading as="h2" size="md" p={4}>
+                      {post.title.slice(0, 50)}
+                      {post.title.length > 50 ? "..." : ""}
+                    </Heading>
+                  </Box>
+                </Flex>
+                <Text
+                  className="blog-card-date"
+                  fontSize="md"
+                  color="gray.500"
+                  position="absolute"
+                  bottom={0}
+                  right={0}
+                  p={4}
+                >
+                  {new Date(post.createdAt).toLocaleString("ja-JP", {
+                    timeZone: "Asia/Tokyo",
+                  })}
+                </Text>
+              </Link>
+            </Box>
+          );
         })}
-     </Grid>
+      </Grid>
     </Box>
- );
+  );
 }
--- src/app/gallery/_GalleryList.ts
+++ src/app/gallery/_GalleryList.ts
@@ -1,56 +1,56 @@
 import type GalleryItem from "./_GalleryItem";
 
 const galleryList: GalleryItem[] = [
-    {
-      image: "/gallery1.jpg",
-      title: "",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery2.jpg",
-      title: "２回使ってどこかにいった",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery3.jpg",
-      title: "",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery4.jpg",
-      title: "",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery5.jpg",
-      title: "",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery6.jpg",
-      title: "AizuHack2023 Web賞",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery7.jpg",
-      title: "",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery8.jpg",
-      title: "",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery9.jpg",
-      title: "AizuHack2023 Web",
-      createdOn: "2023-10-20",
-    },
-    {
-      image: "/gallery10.jpg",
-      title: "AizuHack2022 LineBot",
-      createdOn: "2023-10-20",
-    },
-  ];
+  {
+    image: "/gallery1.jpg",
+    title: "",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery2.jpg",
+    title: "２回使ってどこかにいった",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery3.jpg",
+    title: "",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery4.jpg",
+    title: "",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery5.jpg",
+    title: "",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery6.jpg",
+    title: "AizuHack2023 Web賞",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery7.jpg",
+    title: "",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery8.jpg",
+    title: "",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery9.jpg",
+    title: "AizuHack2023 Web",
+    createdOn: "2023-10-20",
+  },
+  {
+    image: "/gallery10.jpg",
+    title: "AizuHack2022 LineBot",
+    createdOn: "2023-10-20",
+  },
+];
 
 export default galleryList;
