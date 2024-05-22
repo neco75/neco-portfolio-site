e0106c0ab97de05756c42098231791646554e5c3
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-05-23 03:45:29 +0900

    skill update

--- src/app/skill/_data.ts
+++ src/app/skill/_data.ts
@@ -5,35 +5,35 @@ export const skills: Skill[] = [
       name: "HTML",
       level: "☆☆★★★",
       description:
-          "ちょっと分かる",
+          "基本的なタグは理解している。",
   },
   {
       name: "CSS",
+      level: "☆★★★★",
+      description:
+          "本当に基本的なものしか覚えていない。普段はTailwindCSSを使用。SCSSは本当に少し触れたことがある。",
+  },
+  {
+      name: "JavaScript/TypeScript",
       level: "☆☆★★★",
       description:
-          "ちょっと分かるかもしれない",
+          "勉強中。",
   },
   {
-      name: "JavaScript",
-      level: "☆★★★★",
-      description:
-          "理解したい",
-  },
+    name: "jQuery",
+    level: "☆★★★★",
+    description:
+        "授業やバイトで使用。",
+},
   {
-      name: "TypeScript",
-      level: "☆★★★★",
+      name: "React/Next.js",
+      level: "☆☆★★★",
       description:
-          "勉強中。Typescriptのありがたみ分かっていない",
-  },
-  {
-      name: "Next.js",
-      level: "☆★★★★",
-      description:
-          "勉強中。このサイト実装できるくらい",
+          "勉強中。このサイトを実装できるくらい。Next.jsから勉強し始めたため素のReactはあまり触れていない。Next.jsは基本的にAppRouteを使っている。",
   },
   {
       name: "C言語",
-      level: "☆☆★★★",
+      level: "☆★★★★",
       description:
           "授業で勉強",
   },
@@ -41,6 +41,18 @@ export const skills: Skill[] = [
       name: "Java",
       level: "☆★★★★",
       description:
-          "バイト先で勉強。ThymeleafやSpringFrameworkなども勉強",
+          "授業やバイト先で使用。ThymeleafやSpringFrameworkなども合わせて使用。研究室でJavaを使用した教育支援システムを開発予定。",
+  },
+  {
+    name: "Python",
+    level: "☆☆★★★",
+    description:
+        "授業や個人開発で使用。Flaskを用いたWebアプリケーション開発(LINE Bot)も経験。",
+  },
+  {
+    name: "SQLite3",
+    level: "☆☆★★★",
+    description:
+        "個人開発で使用。調べながらであれば基本的な操作はできる。",
   },
 ];
