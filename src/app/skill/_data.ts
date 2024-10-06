41bf559d5d9a423074b7a92dda09b091af80fcc0
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-10-07 02:24:03 +0900

    skill update

--- src/app/skill/_data.ts
+++ src/app/skill/_data.ts
@@ -2,57 +2,52 @@ import { Skill } from "./_types";
 
 export const skills: Skill[] = [
   {
-      name: "HTML",
-      level: "☆☆★★★",
-      description:
-          "基本的なタグは理解している。",
+    name: "HTML",
+    level: "☆☆★★★",
+    description: "基本的なタグは理解している。",
   },
   {
-      name: "CSS",
-      level: "☆★★★★",
-      description:
-          "本当に基本的なものしか覚えていない。普段はTailwindCSSを使用。SCSSは本当に少し触れたことがある。",
+    name: "CSS",
+    level: "☆★★★★",
+    description:
+      "本当に基本的なものしか覚えていない。普段はTailwindCSSを使用。SCSSは本当に少し触れたことがある。",
   },
   {
-      name: "JavaScript/TypeScript",
-      level: "☆☆★★★",
-      description:
-          "勉強中。",
+    name: "JavaScript/TypeScript",
+    level: "☆☆★★★",
+    description: "勉強中。",
   },
   {
     name: "jQuery",
     level: "☆★★★★",
+    description: "授業やバイトで使用。",
+  },
+  {
+    name: "React/Next.js",
+    level: "☆☆★★★",
     description:
-        "授業やバイトで使用。",
-},
-  {
-      name: "React/Next.js",
-      level: "☆☆★★★",
-      description:
-          "勉強中。このサイトを実装できるくらい。Next.jsから勉強し始めたため素のReactはあまり触れていない。Next.jsは基本的にAppRouteを使っている。",
+      "勉強中。このサイトを実装できるくらい。Next.jsから勉強し始めたため素のReactはあまり触れていない。Next.jsは基本的にAppRouteを使っている。オンラインプログラミングスクール(TexhTrain)のフロントエンドコースでReactを学習中。",
   },
   {
-      name: "C言語",
-      level: "☆★★★★",
-      description:
-          "授業で勉強",
+    name: "C言語",
+    level: "☆★★★★",
+    description: "授業で勉強",
   },
   {
-      name: "Java",
-      level: "☆★★★★",
-      description:
-          "授業やバイト先で使用。ThymeleafやSpringFrameworkなども合わせて使用。研究室でJavaを使用した教育支援システムを開発予定。",
+    name: "Java",
+    level: "☆★★★★",
+    description:
+      "大学やバイト先で使用。ThymeleafやSpringFrameworkなども合わせて使用。研究室でサーブレット&JSPを使ったシステム開発を行うので勉強中。就業型インターンに参加した際Thymeleafを使用。",
   },
   {
     name: "Python",
     level: "☆☆★★★",
     description:
-        "授業や個人開発で使用。Flaskを用いたWebアプリケーション開発(LINE Bot)も経験。",
+      "授業や個人開発で使用。Flaskを用いたWebアプリケーション開発(LINE Bot)も経験。",
   },
   {
     name: "SQLite3",
     level: "☆☆★★★",
-    description:
-        "個人開発で使用。調べながらであれば基本的な操作はできる。",
+    description: "個人開発で使用。調べながらであれば基本的な操作はできる。",
   },
 ];
